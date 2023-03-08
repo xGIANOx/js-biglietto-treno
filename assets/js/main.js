bf8252fe2b3ec7c69b6a14e1179aad8767ad0287
xGIANOx <studioenricogian@gmail.com>
2023-03-08 16:02:01 +0100

    [add] comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,35 @@
+// user km input
 const userKm = prompt("Quanti chilometri percorrerai?");
 console.log(userKm);
+
+// km on screen print
 
 let chilometers = document.getElementById("km");
 chilometers.innerHTML = "Percorrerai " + userKm + " chilometri"
 
+// user age input
+
 const userAge = prompt("qual è la tua età?");
 console.log(userAge);
+
+// age on screen print
 
 let yourAge = document.getElementById("user_age");
 yourAge.innerHTML = "Hai " + userAge + " anni"
 
+// €/Km
+
 let kmPrice = 0.21;
+
+// price per km calculator
 
 const price = userKm * kmPrice;
 
 console.log(price);
 
 let normalPrice = document.getElementById("total")
+
+// discount section
 
 let discountTwenty = (price / 100) * 20;
 let finalPrice_twenty = price - discountTwenty;
@@ -31,7 +44,7 @@ let finalPrice_fourty = price - discountFourty;
 let final_40 = document.getElementById("total");
 
 
-
+// Print total price discounted on age
 
 
 if (userAge < 18){
